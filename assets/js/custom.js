99ad63838debfa61d84467bbb3a5d8e965849a32
jgmuchiri <jgmuchiri@gmail.com>
2019-01-10 13:21:03 -0500

    Initial update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,3 @@
-$('document').ready( function() {
-    var form = $('#verify-envato-purchase');
-
-    $('#verify-envato-purchase').submit(function(e){
-        e.preventDefault();
-
-        $.ajax( {
-            type: "POST",
-            url: form.attr( 'action' ),
-            data: form.serialize(),
-            success: function( response ) {
-                $('#show-result').html(response);
-            }
-        } );
-    } );
-} );
 function deletePurchases() {
     return  confirm('Are you sure?');
 }
